64f586946c6b098ee8124926f0bcf4d23a58f2aa
minseoooooo <oesnimnos@gmail.com>
2024-10-10 22:21:28 +0900

    feat: recommendation 기능 구현 완료
    음식 설명과 함께 이미지 전송하는 부분 추가 구현 완료

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 import openai
 import os
+import re
+import requests
+import xml.etree.ElementTree as ET
 from starlette.middleware.cors import CORSMiddleware
 
 from app.config import app  # 여기서 app을 import
 from app.chatbot import get_chat_response
+
+from bs4 import BeautifulSoup
 
 # 랭체인 관련 import들
 from langchain_openai import ChatOpenAI
@@ -18,6 +23,127 @@ app = FastAPI()
 
 # 환경 변수 로드 (API 키 등)
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def search_ingredients(dish_name):
+    model = ChatOpenAI()
+
+    chat_prompt = ChatPromptTemplate.from_messages([
+        ("system",
+         "Translate a korean dish name in korean without any explanation. Your answer MUST be a one korean word. Examples - Q:Kimchi jjigae A:김치찌개, Q:Tteokbokki A:떡볶이"),
+        ("user", "Q:{dish_name} A:"),
+    ])
+
+    chain = chat_prompt | model | StrOutputParser()
+
+    response = chain.invoke({"dish_name": f"{dish_name}", })
+
+    url = 'http://apis.data.go.kr/1390802/AgriFood/FdFood/getKoreanFoodFdFoodList'
+    myKey = 'API key'
+    params = {'serviceKey': '6KMoh6rjEGBq/v8QvaX/3/KAj0DppT17EgLbwzR1IrrWDX+yiTuMtBEgo35a9fgZHz+5aW/wzd0Kv4RDo7Zuyg==',
+              'service_Type': 'xml', 'Page_No': '1', 'Page_Size': '20', 'food_Name': response}
+
+    ingredients_response = requests.get(url, params=params)
+
+    xml_data = ingredients_response.content
+    root = ET.fromstring(xml_data)
+
+    result_msg_element = root.find('.//result_Msg')
+
+    if result_msg_element is not None and result_msg_element.text == '요청 데이터 없음':
+        return ""
+    else:
+        item = root.find('body/items').findall('item')[0]
+        food_List = item.find('food_List').findall('food')
+
+        ingredients = ""
+        count_item = 0
+
+        for food in food_List:
+            fd_Eng_Nm = food.find('fd_Eng_Nm').text
+            ingredient = fd_Eng_Nm.split(',')[0]
+
+            if count_item == 0:
+                ingredients = ingredient
+            else:
+                ingredients = ingredients + ", " + ingredient
+                if count_item == 4: break
+
+            count_item += 1
+
+        return "Main ingredients are " + ingredients
+
+
+def google_search(query):
+    search_url = f"https://serpapi.com/search.json?q={query}&api_key=d123f6ebd427f365cdab180754399edcd536d81fa81a13454ae4c17f4d700f04"
+    response = requests.get(search_url)
+    return response.json()
+
+
+def scrape_website(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    directions_section = soup.find('h2', string='Directions')  # 'Directions' 섹션 찾기
+    recipe_list = directions_section.find_all_next('ol')  # 모든 <ol> 찾기
+    recipe = ""
+
+    if directions_section is not None:
+        recipe_list = directions_section.find_all_next('ol')  # 모든 <ol> 찾기
+
+        for i in range(len(recipe_list)):
+            if i == len(recipe_list) - 1: break
+            list_items = recipe_list[i].find_all('li')  # <ol> 안의 모든 <li> 추출
+            recipe += f"\n#{i + 1}"
+            for i, item in enumerate(list_items, 1):
+                recipe += f"\n{i}. {item.get_text(strip=True)}"
+
+    return recipe
+
+
+def search_recipe(dish_name):
+    search_results = google_search(f"How to cook {dish_name}")
+    url = next((result['link'] for result in search_results['organic_results'] if
+                result['link'].startswith('https://www.maangchi.com')), None)
+
+    if url is None:
+        return ""
+
+    recipe = scrape_website(url)
+
+    return "Generate the image based on the recipe below:" + recipe
+
+
+def dishimg_gen(dish_name) -> bytes:  # 바이트 스트림 타입을 return
+
+    dish_name = dish_name.replace("[", "").replace("]", "")  # dish_name에서 대괄호 제거
+    sd_prompt = f"A realistic image of {dish_name}"  # 프롬프트
+
+    # sd_prompt에 재료 및 레시피 추가
+    sd_prompt += search_ingredients(dish_name)
+    sd_prompt += search_recipe(dish_name)
+
+    # Stability AI API 요청
+    response = requests.post(
+        f"https://api.stability.ai/v2beta/stable-image/generate/ultra",
+        headers={
+            "authorization": f"sk-TIMRKlFafzEJGCro7de6QVt27PB1ufe8m2w6B1JwpLMUJxO5",
+            "accept": "image/*"
+        },
+        files={"none": ''},
+        data={
+            "prompt": sd_prompt,
+            # "output_format": "png",
+        },
+    )
+
+    if response.status_code == 200:  # 응답이 성공적이면 바이트 스트림을 바로 return (파일로 변환한 후 저장하는 코드 x)
+        return response.content
+    # filename = dish_name.lower().replace(" ", "")
+    # with open(f"./{filename}_test.txt", 'wb') as file:
+    #   file.write(response.content)
+
+    else:
+        raise Exception(str(response.json()))
 
 
 # WebSocket 핸들러
@@ -69,7 +195,7 @@ async def websocket_endpoint(websocket: WebSocket):
       Follow the steps below:
       1. Start the conversation and ask which type of dish the user wants to try.
       2. Based on the user's answer and user's dietary restrictions, suggest a dish what the user can eat for the meal. 
-         In this step, YOU MUST START YOUR OUTPUT WITH "[THE DISH NAME IN ENGLISH]". For example, "[Kimchi Stew]"". Then explain the dish in detail.
+         In this step, YOU MUST START YOUR OUTPUT WITH "[THE DISH NAME IN ENGLISH]". Then explain the dish in detail. For example, "[Kimchi Stew] Kimchi Stew(Kimchi Jjigae) is ...(continue)...."
       3. If the user don't like the suggestion, go back to step 2.
       4. If the user decide what to eat, end the conversation.  
       """
@@ -89,10 +215,15 @@ async def websocket_endpoint(websocket: WebSocket):
             response = chain.invoke({"messages": chat_history.messages})  # recommendation 플로우에선 챗봇이 먼저 말함
             chat_history.add_ai_message(response.content)
 
-            # if response.content.startswith("["):
-            #     dish_name = re.search(r'\[([\D]+)\]', response.content).group(1)
-            #     dishimg_gen(dish_name)
-            #     response.content = response.content[len(dish_name) + 2:].lstrip()
+            if response.content.startswith("["):  # 메뉴 이미지 생성하는 코드
+                dish_name = re.search(r'\[([\D]+)\]', response.content).group(1)
+                image_bytes = dishimg_gen(dish_name)
+                response.content = response.content[len(dish_name) + 2:].lstrip()
+
+                try:
+                    await websocket.send_bytes(image_bytes)  # 바이너리 데이터 전송
+                except Exception as e:
+                    await websocket.send_text(f"Error: {str(e)}")
 
             await websocket.send_text(response.content)  # 챗봇이 한 말 send
 
